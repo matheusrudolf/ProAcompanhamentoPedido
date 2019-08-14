2f5097d9bae1bf82c40b6245fd53475d789634ef
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-13 20:27:55 -0400

    Criação das primeiras Views e Controllers

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMvcControllersActions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewResult ViewResultado = new ViewResult()
+            {
+                ViewName = "Resultado",
+                ViewData = new ViewDataDictionary(
+                    new EmptyModelMetadataProvider(),
+                    new ModelStateDictionary())
+                {
+                    Model = $"Home Controller"
+                }
+            };
+            return ViewResultado;
+        }
+    }
+}
