3a212284107d640e7e6b4f2f9272b889bde52119
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-13 21:32:41 -0400

    Correções

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+using ProAcompanhamentoPedido.Models;
+using ProAcompanhamentoPedido.Repositorio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,19 +13,17 @@ namespace AspNetCoreMvcControllersActions.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        private readonly AcompanhamentoContexto _context;
+
+        public HomeController(AcompanhamentoContexto context)
         {
-            ViewResult ViewResultado = new ViewResult()
-            {
-                ViewName = "Resultado",
-                ViewData = new ViewDataDictionary(
-                    new EmptyModelMetadataProvider(),
-                    new ModelStateDictionary())
-                {
-                    Model = $"Home Controller"
-                }
-            };
-            return ViewResultado;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Pedido> pedidos = await _context.Pedidos.Include(p => p.Cliente).ToListAsync();
+            return View(pedidos);
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -5,33 +5,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ProAcompanhamentoPedido
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
         {
-            services.AddMvc();
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void ConfigureServices(IServiceCollection services)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseStaticFiles();
-
-            app.UseMvc(rotas =>
-            {
-                rotas.MapRoute(name: "default", template: "{controller=Home}/{action=Index}");
-            });
+            services.AddMvc()
+            services.AddEntityFrameworkNpgsql()
         }
     }
 }
