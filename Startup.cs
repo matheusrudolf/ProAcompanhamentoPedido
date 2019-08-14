3747ae5a55ddc1daa783061f181117ba096849ac
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-13 21:44:55 -0400

    Desenvolvido o Index, Layout e ViewImports

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ProAcompanhamentoPedido.Repositorio;
 
 namespace ProAcompanhamentoPedido
 {
@@ -21,8 +22,10 @@ namespace ProAcompanhamentoPedido
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
+            services.AddMvc();
             services.AddEntityFrameworkNpgsql()
+            .AddDbContext<AcompanhamentoContexto>
+            (options => options.UseNpgsql(Configuration.GetConnectionString("Conexao")));
         }
     }
 }
