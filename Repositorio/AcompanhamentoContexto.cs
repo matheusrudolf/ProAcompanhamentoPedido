3e42a04d02d46907d7594a9e805ded5cd0010e24
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-13 20:58:13 -0400

    Desenvolvido Repositorio do Projeto

---
+++ Repositorio/AcompanhamentoContexto.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProAcompanhamentoPedido.Dominio;
+using ProAcompanhamentoPedido.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAcompanhamentoPedido.Repositorio
+{
+    public class AcompanhamentoContexto : DbContext
+    {
+        public AcompanhamentoContexto(DbContextOptions<AcompanhamentoContexto> opcoes) : base(opcoes)
+        {
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+
+            modelBuilder.Entity<Pedido>()
+                .HasOne(p => p.Cliente)
+                .WithMany(c => c.Pedidos)
+                .HasForeignKey(p => p.ClienteId);
+
+            modelBuilder
+            .Entity<Pedido>()
+            .Property(p => p.Status)
+            .HasConversion(
+            v => v.ToString(),
+            v => (DomStatusPedido)Enum.Parse(typeof(DomStatusPedido), v));
+        }
+    }
+}
